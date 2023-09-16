b246c5083bda6c20cc4e774524abba4376c90401
Ramdani Syaputra <rmdn.syaputra@gmail.com>
2023-09-16 12:32:52 +0700

    first commit

---
+++ app/Http/Controllers/API/ApiController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class ApiController extends Controller
+{
+    public function firstApi()
+    {
+        $data = [
+            [
+                'title' => 'Judul 1',
+                'image' => Storage::disk('public')->url('images/image1.png'),
+                'description' => '',
+            ],
+            [
+                'title' => 'Judul 2',
+                'image' => Storage::disk('public')->url('images/image2.png'),
+                'description' => 'Deskripsi 2',
+            ],
+            [
+                'title' => 'Judul 3',
+                'image' => Storage::disk('public')->url('images/image3.png'),
+                'description' => 'Deskripsi 3',
+            ],
+            [
+                'title' => 'Judul 4',
+                'image' => Storage::disk('public')->url('images/image4.png'),
+                'description' => 'Deskripsi 4',
+            ],
+            [
+                'title' => 'Judul 5',
+                'image' => Storage::disk('public')->url('images/image5.png'),
+                'description' => 'Deskripsi 5',
+            ],
+        ];
+
+        return response()->json($data);
+    }
+
+}
